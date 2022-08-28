c680dd1e3a79ca5e55d5b62c7adf7cecd19fd1cd
akemiyxl <akemiyxl.github@gmail.com>
2022-08-28 21:03:09 +0530

    ColorPicker do not use forceShowLedLight actions
    *  cannot find symbol mNoMan.forceShowLedLight(color);
    
    Signed-off-by: Anush02198 <anush.4376@gmail.com>
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- src/com/proton/support/colorpicker/ColorPickerDialog.java
+++ src/com/proton/support/colorpicker/ColorPickerDialog.java
@@ -126,15 +126,19 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
     }
 
     private void showLed(int color) {
-        if (mShowLedPreview) {
-            mNoMan.forceShowLedLight(color);
-        }
+        // if (mShowLedPreview) {
+        //     if (color == 0xFFFFFFFF) {
+        //         // argb white doesn't work
+        //         color = 0xffffff;
+        //     }
+        //     mNoMan.forceShowLedLight(color);
+        // }
     }
 
     private void switchOffLed() {
-        if (mShowLedPreview) {
-            mNoMan.forceShowLedLight(0);
-        }
+        // if (mShowLedPreview) {
+        //     mNoMan.forceShowLedLight(-1);
+        // }
     }
 
     void setAlphaSliderVisible(boolean visible) {

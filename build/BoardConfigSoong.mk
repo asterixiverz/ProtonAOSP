feab1d9052acdf8c4fa91a0446db3b5c7d794524
mgs28-mh <gsaputra880@gmail.com>
2024-11-17 15:12:33 +0000

    build: fixup!

--- build/BoardConfigSoong.mk
+++ build/BoardConfigSoong.mk
@@ -3,4 +3,4 @@ SOONG_CONFIG_aospVars += target_init_vendor_lib
 SOONG_CONFIG_aospVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 
 SOONG_CONFIG_aospVars += qcom_um_namespace
-SOONG_CONFIG_aospVars_qcom_um_namespace := vendor/qcom/opensource/commonsys-intf # change where display_intf_headers is
+SOONG_CONFIG_aospVars_qcom_um_namespace := vendor/qcom/opensource/commonsys-intf/display # change where display_intf_headers is

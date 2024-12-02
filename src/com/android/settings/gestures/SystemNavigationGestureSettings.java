Wave-Project/packages_apps_Settings

Settings: Add navbar layout inversion toggle [2/2]
Change-Id: I01186251a33cc501e3743122995e0ee2b3128b42
Signed-off-by: rituj <ritujbeniwal@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -145,6 +145,10 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_SETTING);
             p.setExtraWidgetOnClickListener((v) -> startActivity(new Intent(
                     GestureNavigationSettingsFragment.GESTURE_NAVIGATION_SETTINGS)));
+        } else if (info.getKey() == KEY_SYSTEM_NAV_3BUTTONS) {
+            p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_SETTING);
+            p.setExtraWidgetOnClickListener((v) -> ThreeButtonNavigationInvertDialog
+                    .show(this));
         } else {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_GONE);
         }

asterixiverz/ProtonAOSP

Test: manual

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -153,6 +153,9 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         if (info.getKey() == KEY_SYSTEM_NAV_GESTURAL) {
             pref.setExtraWidgetOnClickListener((v) -> startActivity(new Intent(
                     GestureNavigationSettingsFragment.GESTURE_NAVIGATION_SETTINGS)));
+        } else if (info.getKey() == KEY_SYSTEM_NAV_3BUTTONS) {
+            pref.setExtraWidgetOnClickListener((v) -> ThreeButtonNavigationInvertDialog
+                    .show(this));
         }
     }
 
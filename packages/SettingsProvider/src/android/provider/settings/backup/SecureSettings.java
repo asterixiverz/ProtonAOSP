ArrowOS/android_frameworks_base

base: SystemUI: Add Black theme [1/4]
[palaych: Add settings key into backup and validators]

Author: Pranav Vashi <neobuddy89@gmail.com>
Date:   Sat Apr 16 10:38:36 2022 +0530

    Fixes and improvements for Black Theme

    * Add to SettingsObserver for observing toggle behavior.
    * Apply only with dark theme - looks awful on light mode.
    * Add configuration listener to reapply theme when dark mode changes.

    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

Change-Id: Ib165e7ba0288eaad7a1e3c1109638acdb66fa05c
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -193,5 +193,6 @@ public class SecureSettings {
         Settings.Secure.LOCKSCREEN_SHOW_WALLET,
         Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.SYSTEM_BLACK_THEME,
     };
 }

asterixiverz/ProtonAOSP

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -194,6 +194,7 @@ public class SecureSettings {
         Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK,
         Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
-        Settings.Secure.POWER_MENU_HIDE_ON_SECURE
+        Settings.Secure.POWER_MENU_HIDE_ON_SECURE,
+        Settings.Secure.SYSTEM_BLACK_THEME
     };
 }
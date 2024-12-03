ArrowOS/android_frameworks_base

ScreenshotTile: Improve code
Change-Id: Ie130e84971cf8f42a52350d7bbabfb1d9feb52ad

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -88,10 +88,9 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         mHost.collapsePanels();
 
         //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        ArrowUtils.takeScreenshot(mRegion ? false : true);
+        mUiHandler.postDelayed(() -> {
+            ArrowUtils.takeScreenshot(mRegion ? false : true);
+        }, 1000/*timeout*/);
     }
 
     @Override

asterixiverz/ProtonAOSP

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -88,10 +88,9 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         mHost.collapsePanels();
 
         //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        AddOnUtils.takeScreenshot(mRegion ? false : true);
+        mUiHandler.postDelayed(() -> {
+            AddOnUtils.takeScreenshot(mRegion ? false : true);
+        }, 1000/*timeout*/);
     }
 
     @Override